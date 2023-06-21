f6ee65a1939e082be05907139571e174c70a8d49
DennisCatana <dennis.joel235@outlook.es>
2023-06-21 10:51:25 -0500

    Menu y Clase padre

--- src/Felinos.java
+++ src/Felinos.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Felinos {
+public class Felinos {
+    float tamano;
+    String correr;
+    String ronronear;
+
+
+    //Constructor super
+    public Felinos(){
+        super();
+    }
+    //Constructor
+    public Felinos(float tamano, String correr, String ronronear) {
+        this.tamano = tamano;
+        this.correr = correr;
+        this.ronronear = ronronear;
+    }
+
+
+    //Setters and getters
+    public float getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(float tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getCorrer() {
+        return correr;
+    }
+
+    public void setCorrer(String correr) {
+        this.correr = correr;
+    }
+
+    public String getRonronear() {
+        return ronronear;
+    }
+
+    public void setRonronear(String ronronear) {
+        this.ronronear = ronronear;
+    }
+    public void cazar(){
+        System.out.println("Este es el método cazar");
+    }
+    public void correr(){
+        System.out.println("Este es el método correr");
+    }
+    public void rugir(){
+        System.out.println("Este es el método rugir");
+    }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class Menu {
+import java.util.Scanner;
+public class Menu {
+    static Scanner scanner = new Scanner(System.in);
+    static int opcion = 0;
+    public static void main(String[] args) {
+        while (opcion != 7){
+            opcion = menuPrincipal();
+            switch (opcion){
+                case 1:
+                    System.out.println("Caso 1");
+                    break;
+                case 2:
+                    System.out.println("Caso 2");
+                    break;
+                case 3:
+                    System.out.println("Caso 3");
+                    break;
+                case 4:
+                    System.out.println("Caso 4");
+                    break;
+                case 5:
+                    System.out.println("Caso 5");
+                    break;
+                case 6:
+                    System.out.println("Caso 6");
+                    break;
+                case 7:
+                    System.out.println("Saliendo ...");
+                    break;
+                default:
+
+            }
+        }
+    }
+    public static int menuPrincipal(){
+        System.out.println("\n---------- Menú de Felinos ---------\n");
+        System.out.println("En este menu contamos con 6 felinos de diferentes especies\n");
+        System.out.println("1. Jaguar ");
+        System.out.println("2. Pantera");
+        System.out.println("3. León ");
+        System.out.println("4. Bombay ");
+        System.out.println("5. Azul Ruso ");
+        System.out.println("6. Sphynx ");
+        System.out.println("7. Salir \n");
+        System.out.print("Selecione una opción: ");
+        opcion = scanner.nextInt();
+        while (opcion < 1 || opcion > 7) {
+            System.out.println("\n=================================================================================\n");
+            System.out.println("!!Ingrese una opción válida¡¡");
+            System.out.println("=================================================================================\n");
+            System.out.println("\nPor favor vuelva a intentarlo");
+            System.out.println("=================================================================================\n");
+            opcion = menuPrincipal();
+            System.out.print("\nDígite la opción: ");
+            opcion = scanner.nextInt();
+        }
+        return opcion;
+    }
 }
